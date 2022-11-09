c491b5316922c1bb76e0005c5df37733c7d5d5ba
Sarah <vanwars@gmail.com>
2022-11-09 06:29:22 -0500

    first commit

---
+++ PrimNode.java
@@ -0,0 +1,33 @@
+public class PrimNode extends JSONNodeHelper {
+	public PrimNode p = null;
+	public Integer key = Integer.MAX_VALUE;
+	public Integer id;
+	public String label;
+	
+	
+	public PrimNode(Integer id) {
+		this.id = id;
+		this.label = id.toString();
+	}
+	
+	public PrimNode(Integer id, String label) {
+		this.id = id;
+		this.label = label;
+	}
+	
+	@Override
+	public String toString() {
+		String str = "Node: " + this.label;
+		if (this.key == Integer.MAX_VALUE) {
+			str += " (weight: ∞)\t";
+		} else {
+			str += " (weight: " + this.key + ")\t";
+		}
+		if (this.p != null) {
+			str += "--> " + this.p.label;
+		} else {
+			str += "--> (null)";
+		}
+		return str;
+	}
+}
